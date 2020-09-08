8658b3655efcefc5c05b5b85fbc730b6d5cc6834
Yan Paiva <paivayan2015@gmail.com>
2020-09-08 17:29:37 -0300

    menu_mobile

---
+++ Menu_mobile_js_pure/js/main.js
@@ -0,0 +1,16 @@
+const iconMenu = document.querySelectorAll('.icon-menu')
+
+iconMenu[0].addEventListener('click', ()=>{
+  let menu = document.getElementById('menu')
+
+  if(menu.classList.contains('hide')){
+     menu.classList.add('show')
+     menu.classList.remove('hide')
+
+  }else{
+      menu.classList.add('hide')
+      menu.classList.remove('show')
+  }
+
+ 
+})
---
+++ Menu_mobile_js_pure/readme.md
@@ -0,0 +1,5 @@
+<h2>#Menu_Mobile</h2>
+
+<p>mobile menu using only JavaScript</p>
+
+<h3>ScreenShoot</h3>
